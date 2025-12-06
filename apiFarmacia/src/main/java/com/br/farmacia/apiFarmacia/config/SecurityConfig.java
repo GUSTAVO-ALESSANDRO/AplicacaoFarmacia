5692e6cf6f8510637868ee58928924e6c07dc427
Gustavo Alessandro <gualessandro2017@gmail.com>
2025-12-05 21:06:29 -0300

    Atualização do CORS

--- apiFarmacia/src/main/java/com/br/farmacia/apiFarmacia/config/SecurityConfig.java
+++ apiFarmacia/src/main/java/com/br/farmacia/apiFarmacia/config/SecurityConfig.java
@@ -78,7 +78,8 @@ public class SecurityConfig {
         configuration.setAllowedOriginPatterns(Arrays.asList(
             "http://localhost:3000", 
             "http://localhost:5173",
-            "http://localhost:5174"
+            "http://localhost:5174",
+            "https://aplicacao-farmacia.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
